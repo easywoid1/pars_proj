c9adb1dbfa6aa32c1615ea0ae560971e41628290
easywoid0 <gar2719@gmail.com>
2024-10-03 13:31:03 +0500

    configurate main

---
+++ bot/main.py
@@ -0,0 +1,15 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+
+from .config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
