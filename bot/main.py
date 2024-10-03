c2fc80a10f1b55010562a727b5066e6b3c5ee5d3
easywoid0 <gar2719@gmail.com>
2024-10-03 14:06:49 +0500

    add loger and answer on messages

--- bot/main.py
+++ bot/main.py
@@ -2,13 +2,21 @@ import asyncio
 
 from aiogram import Bot, Dispatcher, types
 
-from .config import TOKEN
+from config import TOKEN, logger
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+@dp.message()
+async def get_message(message: types.Message):
+    logger.info(f'message:{message.text}| user_id: {message.from_user.id} | {message.from_user.username}')
+    await message.reply(text="Пожалуйста используйте кнопки-команды для управления ботом")
+
+
 async def main():
+    logger.info('bot start')
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
